3972d57ce5d6a5a35915fbab7b4df756fd9dc0d5
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2018-06-09 14:07:45 +0200

    Remove JSON-LD from rooms DB and scripts, Update rooms DB, Update rooms routes to create JSON-LD response

--- app/routes/rooms.js
+++ app/routes/rooms.js
@@ -1,13 +1,25 @@
 'use strict';
 
 var router = require('../components/router');
+var jsonld = require('../components/jsonld_factory');
 
 function Rooms() {
   var dbc = require('../components/db_client')('rooms');
   var rooms = router(dbc);
 
-  rooms.get('/:id/facilities', (req, res, next) => {
-    res.send(dbc.find(req.params.id)['facilities']);
+  rooms.get('/:id', (req, res, next) => {
+    var entry = dbc.find(req.params.id);
+
+    var json = jsonld.createResource(req.originalUrl, 'Room');
+    json['room_no'] = entry.room_no;
+    json['description'] = entry.description;
+    json['category'] = entry.category;
+    json['size'] = entry.size;
+    json['capacity'] = entry.capacity;
+    json['price'] = entry.price;
+    json['hotel'] = entry.hotel;
+
+    res.send(json);
     next();
   });
 
--- data/base/new_rooms.js
+++ data/base/new_rooms.js
@@ -5,21 +5,13 @@ var hotels = require('../hotels.json');
 
 module.exports = function () {
   var categories = ['Single Room', 'Double Room', 'Triple Room', 'Quadruple Room', 'Quintuple Room'];
-  var collection = {
-    '@context': config.ns + '/contexts/collection',
-    '@type': 'Collection',
-    '@id': config.ns + '/rooms',
-    members: []
-  };
   var rooms = [];
 
   for (var i = 0; i < 1000; i++) {
     var catIndex = Math.floor(Math.random() * categories.length);
 
     rooms.push({
-      '@context': config.ns + '/contexts/room',
-      '@id': config.ns + '/rooms/' + i,
-      '@type': 'Room',
+      id: config.ns + '/rooms/' + i,
       room_no: i + 1,
       description: 'Awesome room',
       category: categories[catIndex],
@@ -28,12 +20,7 @@ module.exports = function () {
       price: parseFloat((Math.random() * 200 + 20).toFixed(2)),
       hotel: config.ns + '/hotels/' + Math.floor(Math.random() * hotels.length)
     });
-    collection.members.push({
-      '@id': config.ns + '/rooms/' + i,
-      '@type': 'vocab:Room'
-    });
   }
 
   w('rooms', JSON.stringify(rooms));
-  w('collections/rooms', JSON.stringify(collection));
 }
