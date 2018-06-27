72710920987407e294996bd40188a1f14d08bf52
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2018-06-27 18:41:54 +0200

    Add creating, updating media

--- app/components/router.js
+++ app/components/router.js
@@ -32,7 +32,6 @@ module.exports = dbc => {
     }
   };
 
-  /* GET REQUESTS */
   router.get('/', (req, res, next) => {
     res.send(jsonld.createCollection(
       req.originalUrl,
@@ -42,29 +41,6 @@ module.exports = dbc => {
     next();
   });
 
-  /* POST REQUESTS */
-  // router.post('/', (req, res, next) => {
-  //   if (dbc.create(req.body)) {
-  //     res.status(201).send('Entry created!');
-  //   } else {
-  //     res.status(500).send('Entry not created!');
-  //   }
-
-  //   next();
-  // });
-
-  /* PUT REQUESTS */
-  // router.put('/', (req, res, next) => {
-  //   if (dbc.update(req.body)) {
-  //     res.status(200).send('Entry updated!');
-  //   } else {
-  //     res.status(404).send('Entry not updated!');
-  //   }
-
-  //   next();
-  // });
-
-  /* DELETE REQUESTS */
   router.delete('/:id', (req, res, next) => {
     if (dbc.remove(req.params.id)) {
       res.sendStatus(200);
--- app/routes/media.js
+++ app/routes/media.js
@@ -10,14 +10,39 @@ function Media() {
   media.get('/:id', (req, res, next) => {
     var entry = dbc.find(req.params.id);
 
-    var json = jsonld.createResource(req.originalUrl, 'Media');
-    json['name'] = entry.name;
-    json['description'] = entry.description;
-    json['url'] = entry.url;
-    json['width'] = entry.width;
-    json['height'] = entry.height
+    if (entry) {
+      var json = jsonld.createResource(req.originalUrl, 'Media');
+      json['name'] = entry.name;
+      json['description'] = entry.description;
+      json['url'] = entry.url;
+      json['width'] = entry.width;
+      json['height'] = entry.height
 
-    res.send(json);
+      res.send(json);
+    } else {
+      res.sendStatus(404)
+    }
+
+    next();
+  });
+
+  media.post('/', (req, res, next) => {
+    dbc.create(req.body, (entity) => {
+      if (entity) res.sendStatus(201);
+      else res.sendStatus(500);
+    });
+
+    next();
+  });
+
+  media.put('/:id', (req, res, next) => {
+    req.body['id'] = req.params.id;
+
+    dbc.update(req.body, (entity) => {
+      if (entity) res.sendStatus(200);
+      else res.sendStatus(500);
+    });
+
     next();
   });
 
